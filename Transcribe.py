94335adc2c01497e2d0f19e099d2828980ec1f72
Jacob Schneider <82042695+wacksientrist@users.noreply.github.com>
2024-01-22 11:08:47 -0500

    Add files via upload

--- Transcribe.py
+++ Transcribe.py
@@ -2,8 +2,7 @@ import speech_recognition as sr
 import chatterbot
 import os
 
-Listener = sr.Recognizer()
-mic = sr.Microphone()
+
 Bot = chatterbot.ChatBot("ChatDot")
 
 def recognize(recognizer, microphone):
@@ -34,16 +33,28 @@ def recognize(recognizer, microphone):
     return response
 
 print("Starting...")
+mode = input("Would you like text or audio input? type '1' for audio and '2' for text: \t")
 
-while True:
-    Query = recognize(Listener, mic)["transcription"]
-    
-    Response = Bot.get_response(Query)
-    print(Query)
-    if Response == None:
-        Response = input("Enter Text")
-    os.system("gtts-cli '"+str(Response)+"' --output Latest.mp3")
-    os.system("mpg321 Latest.mp3")
-    
+if mode == "1":
+    Listener = sr.Recognizer()
+    mic = sr.Microphone()
 
-    print(Response)
+    while True:
+        Query = recognize(Listener, mic)["transcription"]
+        
+        Response = Bot.get_response(Query)
+        print(Query)
+
+        os.system("gtts-cli '"+str(Response)+"' --output Latest.mp3")
+        os.system("mpg321 Latest.mp3")
+        
+
+        print(Response)
+if mode == "2":
+    while True:
+        Query = input("Query: \t")
+        Response = Bot.get_response(Query)
+
+        print(Query)
+        print(Response)
+        
