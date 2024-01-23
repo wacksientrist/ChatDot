d2a420773e0707d01255a270e08f8258de3e43a9
Jacob Schneider <82042695+wacksientrist@users.noreply.github.com>
2024-01-23 09:09:44 -0500

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,7 +1,7 @@
 brew update
 
 brew install python@3.8
-brew install PyAudio
+brew install portaudio
 brew install gcc
 
 python3.8 -m pip install speechrecognition
@@ -9,5 +9,6 @@ python3.8 -m pip install chatterbot
 python3.8 -m pip install chatterbot_corpus
 python3.8 -m pip install gtts
 python3.8 -m pip install numpy==1.22.4
+python3.8 -m pip install pyaudio
 
 brew install mpg321
